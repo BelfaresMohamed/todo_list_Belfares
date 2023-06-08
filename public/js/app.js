14a3532c72acfc8501bec758cfe13d55fa41398d
mohamed <mohammedbelfares@gmail.com>
2023-06-08 18:20:53 +0000

    4 modifications des pages

--- public/js/app.js
+++ public/js/app.js
@@ -18,53 +18,73 @@ button.addEventListener('click', () => {
 
 function rendre() {
     let selectVl = select.value;
-    let selectelemnt;
+    let selectelemnts=[];
 
-    if (selectVl === 'completed') {
-        selectelemnt = elements.filter(element => element.completed);
-    } else if (selectVl === 'uncompleted') {
-        selectelemnt = elements.filter(element => !element.completed);
+    if (selectVl === 'compler') {
+        for (let index = 0; index < elements.length; index++) {
+            const element = elements[index];
+            if (element.completed ==true) {
+              selectelemnts.push(element);
+            }
+          }
+          
+    } else if (selectVl === 'no compler') {
+        for (let index = 0; index < elements.length; index++) {
+            if (!elements[index].completed) {
+              selectelemnts.push(elements[i]);
+            }
+          }
     } else {
-        selectelemnt = elements;
+        selectelemnts = elements;
     }
     box.innerHTML = '';
-    selectelemnt.forEach((element, index) => {
+    for (let index = 0; index < selectelemnts.length; index++) {
+        let element = selectelemnts[index];
+      
         let item = document.createElement('div');
-        item.className = 'box-item' + (element.completed ? ' completed' : '');
-        item.innerHTML = element.value;
-
-
-        // VERIFIER   
+        if (element.completed) {
+            item.className = 'box-item completed';
+          } else {
+            item.className = 'box-item';
+          }
+          
+        item.innerHTML = '<p>' + element.value + '</p>';
+      
+        let actionsDiv = document.createElement('div');
+      
         let completeIcon = document.createElement('span');
         completeIcon.className = 'action-icon';
-        completeIcon.innerHTML = '✔️';
+        completeIcon.innerHTML = '<i class="fa-solid fa-check" style="color: #25511f;"></i>';
         completeIcon.addEventListener('click', () => {
-            elements[index].completed = !elements[index].completed;
-            rendre();
+          elements[index].completed = !elements[index].completed;
+          rendre();
         });
-        // SUPPRIMER
+        actionsDiv.appendChild(completeIcon);
+      
         let deleteIcon = document.createElement('span');
         deleteIcon.className = 'action-icon';
-        deleteIcon.innerHTML = '❌';
+        deleteIcon.innerHTML = '<i class="fa-solid fa-trash" style="color: #ff0000;"></i>';
         deleteIcon.addEventListener('click', () => {
-            elements.splice(index, 1);
-            rendre();
+          elements.splice(index, 1);
+          rendre();
         });
-
+        actionsDiv.appendChild(deleteIcon);
+      
         let editIcon = document.createElement('span');
         editIcon.className = 'action-icon';
-        editIcon.innerHTML = '✏️';
+        editIcon.innerHTML = '<i class="fa-solid fa-marker" style="color: #0b25ea;"></i>';
         editIcon.addEventListener('click', () => {
-            let newValue = prompt('Entrez une nouvelle valeur', elements[index].value);
-            if (newValue !== null) {
-                elements[index].value = newValue.trim();
-                rendre();
-            }
+          let newValue = prompt('Entrez une nouvelle valeur', elements[index].value);
+          if (newValue !== null) {
+            elements[index].value = newValue.trim();
+            rendre();
+          }
         });
-
-        item.appendChild(completeIcon);
-        item.appendChild(deleteIcon);
-        item.appendChild(editIcon);
+        actionsDiv.appendChild(editIcon);
+      
+        item.appendChild(actionsDiv);
+      
         box.appendChild(item);
-    });
+      }
+      
 }
--- public/js/app2.js
+++ public/js/app2.js
@@ -3,138 +3,165 @@ let todo = document.getElementById('todo');
 let doing = document.getElementById('doing');
 let done = document.getElementById('done');
 
-  let todotab = [];
-  let doingtab = [];
-  let donetab = [];
+let todotab = [];
+let doingtab = [];
+let donetab = [];
 
-  function pushtab() {
-      let element = inputValue.value.trim();
-      if (element !== '') {
-          todotab.push(element);
-          inputValue.value = '';
-          modifier();
-      }
-  }
-
-  function supprimer(category, index) {
-      switch (category) {
-          case 'todo':
-              todotab.splice(index, 1);
-              break;
-          case 'doing':
-              doingtab.splice(index, 1);
-              break;
-          case 'done':
-              donetab.splice(index, 1);
-              break;
-      }
-      modifier();
-  }
-
-  function deplacer(category,index) {
-      let element;
-      switch (category) {
-          case 'todo':
-              element = todotab.splice(index, 1);
-              break;
-          case 'doing':
-              element = doingtab.splice(index, 1);
-              break;
-          case 'done':
-              element = donetab.splice(index, 1);
-              break;
-      }
-
-      let category_tb = prompt('Déplacez la tâche vers la catégorie (todo/doing/done):');
-      if (category_tb.trim() === '') {
-          return;
-      }
-
-      switch (category_tb) {
-          case 'todo':
-              todotab.push(element);
-              break;
-          case 'doing':
-              doingtab.push(element);
-              break;
-          case 'done':
-              donetab.push(element);
-              break;
-          default:
-              alert('il na aucun category de ce nom');
-              return;
-      }
-
-      modifier();
-  }
-
-  function modifier() {
+function pushtab() {
+    let element = inputValue.value.trim();
+    if (element !== '') {
+        todotab.push(element);
+        inputValue.value = '';
+        modifier();
+    }
+}
 
 
-      // Efface les éléments précédents
-      todo.innerHTML = '';
-      doing.innerHTML = '';
-      done.innerHTML = '';
+function supprimer(category, index) {
+    if (category === 'todo') {
+        todotab.splice(index, 1);
+    } else {
+        if (category === 'doing') {
+            doingtab.splice(index, 1);
+        } else {
+            if (category === 'done') {
+                donetab.splice(index, 1);
+            }
+        }
+    }
 
-      
-      for (let index = 0; index < todotab.length; index++) {
-          let element = todotab[index];
-          let item = criationitem(element, 'todo', index);
-          todo.appendChild(item);   
-      }
+    modifier();
+}
 
-      for (let index = 0; index < doingtab.length; index++) {
-          let element = doingtab[index];
-          let item = criationitem(element, 'doing', index);
-          doing.appendChild(item);
-      }
+// cette function je veux l'appeler qu'on je peut modifier 
+function modifier() {
 
-      for (let index = 0; index < donetab.length; index++) {
-          let element = donetab[index];
-          let item = criationitem(element, 'done', index);
-          done.appendChild(item);
-      }
-  }
 
-  function criationitem(element, category, index) {
-      let item = document.createElement('div');
-      item.className = 'item';
-      item.innerHTML = element;
-      let deleteIcon = document.createElement('i');
-      deleteIcon.innerHTML = '❌';
-      deleteIcon.addEventListener('click', function () {
-          supprimer(category, index);
-      });
-      let modifier = document.createElement('i');
-      modifier.innerHTML = '✏️';
+    // Efface les éléments précédents
+    todo.innerHTML = '';
+    doing.innerHTML = '';
+    done.innerHTML = '';
 
-      modifier.addEventListener('click', function () {
-          let newValue = prompt('Entrez une nouvelle valeur');
-                    if (newValue !== null) {
-                        element[index].value = newValue;
+
+    for (let index = 0; index < todotab.length; index++) {
+        let element = todotab[index];
+        let item = ajouteritem(element, 'todo', index);
+        todo.appendChild(item);
+    }
+
+    for (let index = 0; index < doingtab.length; index++) {
+        let element = doingtab[index];
+        let item = ajouteritem(element, 'doing', index);
+        doing.appendChild(item);
+    }
+
+    for (let index = 0; index < donetab.length; index++) {
+        let element = donetab[index];
+        let item = ajouteritem(element, 'done', index);
+        done.appendChild(item);
+    }
+}
+
+function deplacer(category, index, newSelectVal) {
+    let element;
+    // stocker l'element supprimer dans la variable
+    if (category === 'todo') {
+        element = todotab.splice(index, 1);
+    } else {
+        if (category === 'doing') {
+            element = doingtab.splice(index, 1);
+        } else {
+            if (category === 'done') {
+                element = donetab.splice(index, 1);
+            }
+        }
+    }
+
+
+
+    if (newSelectVal === 'todo') {
+        todotab.push(element);
+    } else {
+        if (newSelectVal === 'doing') {
+            doingtab.push(element);
+        } else {
+            if (newSelectVal === 'done') {
+                donetab.push(element);
+            } else {
+                return;
+            }
+        }
+    }
+
+
+    modifier();
+}
+
+
+function ajouteritem(element, category, index) {
+    let item = document.createElement('div');
+    item.className = 'item';
+    item.innerHTML = element;
+    let deleteIcon = document.createElement('i');
+    deleteIcon.innerHTML = '<i class="fa-solid fa-trash" style="color: #ff0000;"></i>';
+    deleteIcon.addEventListener('click', function () {
+        supprimer(category, index);
+    });
+    let modifierIcon = document.createElement('i');
+    modifierIcon.innerHTML = '<i class="fa-solid fa-marker" style="color: #0b25ea;"></i>';
+
+    modifierIcon.addEventListener('click', function () {
+
+        let newValue = prompt('Entrez une nouvelle valeur');
+
+        if (newValue !== null) {
+
+
+            if (category === 'todo') {
+                todotab[index] = newValue
+            } else {
+                if (category === 'doing') {
+                    doingtab[index] = newValue
+                } else {
+                    if (category === 'done') {
+                        donetab[index] = newValue
                     }
-      });
-      let deplacerSelect = document.createElement('select');
-      deplacerSelect.className = 'secl';
-      deplacerSelect.addEventListener('change', function () {
-          deplacer(category, index);
-      });
-      let choix1 = document.createElement('option');
-      choix1.value = 'todo';
-      choix1.text = 'À faire';
-      let choix2 = document.createElement('option');
-      choix2.value = 'doing';
-      choix2.text = 'En cours';
-      let choix3 = document.createElement('option');
-      choix3.value = 'done';
-      choix3.text = 'Terminé';
-      deplacerSelect.appendChild(choix1);
-      deplacerSelect.appendChild(choix2);
-      deplacerSelect.appendChild(choix3);
+                }
+            }
 
-      item.appendChild(deleteIcon);
-      item.appendChild(modifier);
-      item.appendChild(deplacerSelect);
+            modifier()
+        }
+    });
 
-      return item;
-  }
+
+
+
+    // creation d'un nouveau select qui peut deplacer à  l'autre tableaux
+    let deplacerSelect = document.createElement('select');
+    deplacerSelect.className = 'secl';
+    deplacerSelect.addEventListener('change', function () {
+        let newSelectVal = document.querySelector(".secl").value
+        deplacer(category, index, newSelectVal);
+    });
+    let choix1 = document.createElement('option');
+    choix1.value = 'todo';
+    choix1.text = 'À faire';
+
+    let choix2 = document.createElement('option');
+    choix2.value = 'doing';
+    choix2.text = 'En cours';
+
+    let choix3 = document.createElement('option');
+    choix3.value = 'done';
+    choix3.text = 'Terminé';
+
+    deplacerSelect.appendChild(choix1);
+    deplacerSelect.appendChild(choix2);
+    deplacerSelect.appendChild(choix3);
+
+    item.appendChild(deleteIcon);
+    item.appendChild(modifierIcon);
+    item.appendChild(deplacerSelect);
+
+    return item;
+}
