e5551b50ae23ed9d0f5a61ca853db4c8e94c5287
mohamed <mohammedbelfares@gmail.com>
2023-06-08 10:37:44 +0000

    fin de js de premier page

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,22 @@
-       let inputValue = document.getElementById('inputValue');
+// recup les elements byId
+let inputValue = document.getElementById('inputValue');
         let addButton = document.getElementById('addButton');
         let filterSelect = document.getElementById('filterSelect');
         let box = document.getElementById('box');
         let elements = [];
 
+
+// push dans le tableaux 
         addButton.addEventListener('click',()=>{
-             value = inputValue.value.trim();
+             value = inputValue.value;
             if (value !== '') {
                 elements.push({ value, completed: false });
-                renderElements();
+                rendre();
                 inputValue.value = '';
             }
         });
 
-        function renderElements() {
+        function rendre() {
             let filterValue = filterSelect.value;
             let filteredElements = filterValue === 'completed' ? elements.filter(element => element.completed) :
                 filterValue === 'uncompleted' ? elements.filter(element => !element.completed) :
@@ -25,22 +28,24 @@
                 item.className = 'box-item' + (element.completed ? ' completed' : '');
                 item.innerHTML = element.value;
                 
+
+                // VERIFIER   
                 let completeIcon = document.createElement('span');
                 completeIcon.className = 'action-icon';
                 completeIcon.innerHTML = '✔️';
                 completeIcon.addEventListener('click', () => {
                     elements[index].completed = !elements[index].completed;
-                    renderElements();
+                    rendre();
                 });
-                
+                // SUPPRIMER
                 let deleteIcon = document.createElement('span');
                 deleteIcon.className = 'action-icon';
                 deleteIcon.innerHTML = '❌';
                 deleteIcon.addEventListener('click', () => {
                     elements.splice(index, 1);
-                    renderElements();
+                    rendre();
                 });
-                
+                //MODIFIER
                 let editIcon = document.createElement('span');
                 editIcon.className = 'action-icon';
                 editIcon.innerHTML = '✏️';
@@ -48,7 +53,7 @@
                     let newValue = prompt('Entrez une nouvelle valeur', elements[index].value);
                     if (newValue !== null) {
                         elements[index].value = newValue.trim();
-                        renderElements();
+                        rendre();
                     }
                 });
                 
